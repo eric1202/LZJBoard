7f8478ae3d25f0f199524992bbebf0ca2a387398
Heyz <zijie.li@heyz.com>
2016-08-22 11:08:24 +0800

    can make a simple audio record

--- LZJBoard/ViewController.m
+++ LZJBoard/ViewController.m
@@ -9,10 +9,17 @@
 #import "ViewController.h"
 #import "MessageBoardCell.h"
 #import "UIView+Common.h"
-@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+#import <iflyMSC/iflyMSC.h>
+#import "IATConfig.h"
+#define XFKEY @"57b6c6d8"
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource,IFlyRecognizerViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) UIButton *chatBtn;
 
+@property (nonatomic, strong) IFlySpeechRecognizer *iFlySpeechRecognizer;
+@property (nonatomic, strong) IFlyRecognizerView  *iflyRecognizerView;
+@property (nonatomic, strong) IFlyDataUploader *uploader;
 @end
 
 @implementation ViewController
@@ -27,6 +34,8 @@
     [self.tableView registerNib:[UINib nibWithNibName:@"MessageBoardCell" bundle:[NSBundle mainBundle]] forCellReuseIdentifier:@"MessageBoardCell"];
     
     [self.view addSubview:self.chatBtn];
+    NSString *initString = [[NSString alloc] initWithFormat:@"appid=%@",XFKEY];
+    [IFlySpeechUtility createUtility:initString];
     
 }
 
@@ -56,7 +65,7 @@
 #pragma mark - chat button
 -(UIButton *)chatBtn{
     if (_chatBtn == nil) {
-        _chatBtn = [[UIButton alloc]initWithFrame:CGRectMake((self.view.selfW-100)/2.0f, self.view.selfH - 50, 100, 30)];
+        _chatBtn = [[UIButton alloc]initWithFrame:CGRectMake((self.view.selfW - 100)/2.0f, self.view.selfH - 50, 100, 30)];
         [_chatBtn setTitle:@"开始记录" forState:(UIControlStateNormal)];
         [_chatBtn setTitleColor:[UIColor redColor] forState:(UIControlStateNormal)];
         _chatBtn.backgroundColor = [UIColor colorWithHexString:@"f0f2f2"];
@@ -67,7 +76,66 @@
 }
 
 - (void)record:(UIButton *)btn{
+    if (!_iFlySpeechRecognizer) {
+        
+        [self initRecognizer];//初始化识别对象
+    }
+    [_iflyRecognizerView start];
+}
+
+- (void)initRecognizer{
+    _iFlySpeechRecognizer = [IFlySpeechRecognizer sharedInstance];
+    [_iFlySpeechRecognizer setParameter:@"" forKey:[IFlySpeechConstant PARAMS]];
+    [_iFlySpeechRecognizer setParameter:@"iat" forKey:[IFlySpeechConstant IFLY_DOMAIN]];
     
+    //view 语音听写
+    _iflyRecognizerView = [[IFlyRecognizerView alloc] initWithCenter:self.view.center];
+    _iflyRecognizerView.delegate = self;
+    [_iflyRecognizerView setParameter:@"iat" forKey:[IFlySpeechConstant IFLY_DOMAIN]];
+    [_iflyRecognizerView setParameter:@"asrview.pcm " forKey:[IFlySpeechConstant ASR_AUDIO_PATH]];
+    
+    IATConfig *instance = [IATConfig sharedInstance];
+    //设置最长录音时间
+    [_iflyRecognizerView setParameter:instance.speechTimeout forKey:[IFlySpeechConstant SPEECH_TIMEOUT]];
+    //设置后端点
+    [_iflyRecognizerView setParameter:instance.vadEos forKey:[IFlySpeechConstant VAD_EOS]];
+    //设置前端点
+    [_iflyRecognizerView setParameter:instance.vadBos forKey:[IFlySpeechConstant VAD_BOS]];
+    //网络等待时间
+    [_iflyRecognizerView setParameter:@"20000" forKey:[IFlySpeechConstant NET_TIMEOUT]];
+    
+    //设置采样率，推荐使用16K
+    [_iflyRecognizerView setParameter:instance.sampleRate forKey:[IFlySpeechConstant SAMPLE_RATE]];
+    if ([instance.language isEqualToString:[IATConfig chinese]]) {
+        //设置语言
+        [_iflyRecognizerView setParameter:instance.language forKey:[IFlySpeechConstant LANGUAGE]];
+        //设置方言
+        [_iflyRecognizerView setParameter:instance.accent forKey:[IFlySpeechConstant ACCENT]];
+    }else if ([instance.language isEqualToString:[IATConfig english]]) {
+        //设置语言
+        [_iflyRecognizerView setParameter:instance.language forKey:[IFlySpeechConstant LANGUAGE]];
+    }
+    //设置是否返回标点符号
+    [_iflyRecognizerView setParameter:instance.dot forKey:[IFlySpeechConstant ASR_PTT]];
+    
+    _uploader = [[IFlyDataUploader alloc]init];
+    
+    
+}
+
+-(void)onError:(IFlySpeechError *)error{
+    NSLog(@"on error : %@" ,error.errorDesc);
+}
+
+-(void)onResult:(NSArray *)resultArray isLast:(BOOL)isLast{
+    
+    NSMutableString *result = [[NSMutableString alloc] init];
+    NSDictionary *dic = [resultArray objectAtIndex:0];
+    
+    for (NSString *key in dic) {
+        [result appendFormat:@"%@",key];
+    }
+    NSLog(@"on results : %@\n last content:%@",resultArray,result);
 }
 
 @end
